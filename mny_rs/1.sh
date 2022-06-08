11c649c715f0b9c8a2f91cda24b4d4128adde100
ml3u17 <mengya.liu@soton.ac.uk>
2022-06-08 19:59:55 +0800

    Add datasets
    Add the original datasets and the instances we generated.

---
+++ mny_rs/1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --nodes=2     # Number of Tasks
+#SBATCH --ntasks=2
+#SBATCH --mem=150G
+#SBATCH --time=4:00:00
+
+module load openmpi/3.0.0/intel
+
+module load python/3.6.4
+module load numpy/1.16.4
+module load pandas
+
+mpirun -n 1 python ride_sharing.py -i 1 -objs ew -g 30 -p 10 : -n 1 python ride_sharing.py -i 1 -objs et -g 30 -p 10 
